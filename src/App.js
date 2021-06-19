58df42c3c30fa19dca0e365140a34ad14cf14284
umer <umerfaroque20@gmail.com>
2021-06-20 00:50:25 +0530

    food menu app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Gallery from "./Gallery";
+import "./App.css";
+export default function App() {
+  return (
+    <>
+      <Gallery/>
+    </>
+  )
+}
---
+++ src/Data.js
@@ -0,0 +1,79 @@
+const Menu = [
+    {
+       id: 1,
+       image: "https://thefamiliarkitchen.com/wp-content/uploads/2019/02/img_3166.jpg",
+       name:"Appam",
+       category: "breakfast",
+       price: "12₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+ 
+   {
+       id: 2,
+       image: "https://img.etimg.com/photo/msid-69999552,quality-100/mughlai-paratha.jpg",
+       name:"Mughlai",
+       category: "evening",
+       price: "20₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+   {
+       id: 3,
+       image: "https://image.freepik.com/free-photo/fried-egg-with-toasted-bread-bacon_1339-121357.jpg",
+       name:"Bread Platter",
+       category: "breakfast",
+       price: "10₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+    {
+       id: 4,
+       image: "https://previews.123rf.com/images/csstockimages/csstockimages1710/csstockimages171003421/87843023-group-of-indian-food-or-indian-thali-south-indian-thali-south-indian-compleat-meal-south-indian-comp.jpg",
+       name:"Lunch Thali",
+       category: "lunch",
+       price: "50₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+     {
+       id: 5,
+       image: "https://i.pinimg.com/originals/49/4a/89/494a89643d2525e9073791c23b0e269b.jpg",
+       name:"Chicken Roll",
+       category: "evening",
+       price: "80₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+      {
+       id: 6,
+       image: "https://i.pinimg.com/736x/77/c7/4e/77c74e44a603a332dbb6308a03737033.jpg",
+       name:"Non-Veg Thali",
+       category: "dinner",
+       price: "180₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 7,
+       image: "https://i.pinimg.com/originals/ce/dd/a8/cedda8bcc98f1c0e71f4e4741f1e83f9.jpg",
+       name:"Sweets",
+       category: "dinner",
+       price: "60₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 8,
+       image: "https://i.ndtvimg.com/i/2017-09/biryani_650x400_71504256828.jpg",
+       name:"Biryani",
+       category: "lunch",
+       price: "60₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+       {
+       id: 9,
+       image: "https://www.orissapost.com/wp-content/uploads/2019/10/Samosa.jpg",
+       name:"Samosa",
+       category: "evening",
+       price: "10₹",
+       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+   },
+];
+
+
+
+export default Menu;
